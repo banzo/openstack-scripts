69544b7e4f69a08ffb8e37a94159adc5ff8b444d
banzo <dupont.sebastien@gmail.com>
2017-10-24 16:47:17 +0200

    Update simple_sfc_vms.sh

--- simple_sfc_vms.sh
+++ simple_sfc_vms.sh
@@ -108,6 +108,107 @@ sudo ip route add ${DEST_IP} dev eth1
 EOF
 done
 
+# DO THE T4 SFC SETUP
+
+neutron net-create sfc-t4
+neutron subnet-create --name sfc-t4-sn --ip-version 4 sfc-t4 11.0.0.0/24
+neutron router-interface-add router1 sfc-t4-sn
+
+# Disable port security (else packets would be rejected when exiting the service VMs)
+neutron net-update --port_security_enabled=False sfc-t4
+
+neutron port-create sfc-t4 --fixed-ip ip-address=11.0.0.11 --name "p1in_t4"
+neutron port-create sfc-t4 --fixed-ip ip-address=11.0.0.12 --name "p1out_t4"
+neutron port-create sfc-t4 --fixed-ip ip-address=11.0.0.21 --name "p2in_t4"
+neutron port-create sfc-t4 --fixed-ip ip-address=11.0.0.22 --name "p2out_t4"
+neutron port-create sfc-t4 --fixed-ip ip-address=11.0.0.31 --name "p3in_t4"
+neutron port-create sfc-t4 --fixed-ip ip-address=11.0.0.32 --name "p3out_t4"
+neutron port-create sfc-t4 --fixed-ip ip-address=11.0.0.101 --name "source_vm_port_t4"
+neutron port-create sfc-t4 --fixed-ip ip-address=11.0.0.102 --name "dest_vm_port_t4"
+
+# SFC VMs
+nova boot --image "${IMAGE}" --flavor "${FLAVOR}" \
+    --key-name "${SSH_KEYNAME}" --security-groups "${SECGROUP}" \
+    --nic port-id="$(neutron port-show -f value -c id p1in_t4)" \
+    --nic port-id="$(neutron port-show -f value -c id p1out_t4)" \
+    sfc-dpi
+nova boot --image "${IMAGE}" --flavor "${FLAVOR}" \
+    --key-name "${SSH_KEYNAME}" --security-groups "${SECGROUP}" \
+    --nic port-id="$(neutron port-show -f value -c id p2in_t4)" \
+    --nic port-id="$(neutron port-show -f value -c id p2out_t4)" \
+    sfc-firewall
+nova boot --image "${IMAGE}" --flavor "${FLAVOR}" \
+    --key-name "${SSH_KEYNAME}" --security-groups "${SECGROUP}" \
+    --nic port-id="$(neutron port-show -f value -c id p3in_t4)" \
+    --nic port-id="$(neutron port-show -f value -c id p3out_t4)" \
+    sfc-firewall-noftp
+
+# Demo VMs
+nova boot --image "${IMAGE}" --flavor "${FLAVOR}" \
+    --key-name "${SSH_KEYNAME}" --security-groups "${SECGROUP}" \
+    --nic port-id="$(neutron port-show -f value -c id source_vm_port_t4)" \
+    bsa_proxy
+nova boot --image "${IMAGE}" --flavor "${FLAVOR}" \
+    --key-name "${SSH_KEYNAME}" --security-groups "${SECGROUP}" \
+    --nic port-id="$(neutron port-show -f value -c id dest_vm_port_t4)" \
+    dest_vm
+
+# HTTP Flow classifier (catch the web traffic from source_vm to dest_vm)
+SOURCE_IP=$(openstack port show source_vm_port_t4 -f value -c fixed_ips|grep "ip_address='[0-9]*\."|cut -d"'" -f2)
+DEST_IP=$(openstack port show dest_vm_port_t4 -f value -c fixed_ips|grep "ip_address='[0-9]*\."|cut -d"'" -f2)
+neutron flow-classifier-create \
+    --ethertype IPv4 \
+    --source-ip-prefix ${SOURCE_IP}/32 \
+    --destination-ip-prefix ${DEST_IP}/32 \
+    --protocol tcp \
+    --destination-port 80:80 \
+    --logical-source-port source_vm_port_t4 \
+    FC_http_t4
+
+# UDP flow classifier (catch all UDP traffic from source_vm to dest_vm, like traceroute)
+neutron flow-classifier-create \
+    --ethertype IPv4 \
+    --source-ip-prefix ${SOURCE_IP}/32 \
+    --destination-ip-prefix ${DEST_IP}/32 \
+    --protocol udp \
+    --logical-source-port source_vm_port_t4 \
+    FC_udp_t4
+
+# Get easy access to the VMs
+route_to_subnetpool
+
+# Create the port pairs for all 3 VMs
+neutron port-pair-create --ingress=p1in_t4 --egress=p1out_t4 PP1_t4
+neutron port-pair-create --ingress=p2in_t4 --egress=p2out_t4 PP2_t4
+neutron port-pair-create --ingress=p3in_t4 --egress=p3out_t4 PP3_t4
+
+# And the port pair groups
+neutron port-pair-group-create --port-pair PP1_t4 --port-pair PP2_t4 PG1_t4
+neutron port-pair-group-create --port-pair PP3_t4 PG2_t4
+
+# The complete chain
+neutron port-chain-create --port-pair-group PG1_t4 --port-pair-group PG2_t4 --flow-classifier FC_udp_t4 --flow-classifier FC_http_t4 PCt41
+
+sleep 5
+
+# Start a basic demo web server
+ssh -o StrictHostKeyChecking=no cirros@${DEST_IP} 'while true; do echo -e "HTTP/1.0 200 OK\r\n\r\nWelcome to $(hostname) t4" | sudo nc -l -p 80 ; done&'
+
+# On service VMs, enable eth1 interface and add static routing
+for sfc_port in p1in_t4 p2in_t4 p3in_t4
+do
+    ssh -o StrictHostKeyChecking=no -T cirros@$(openstack port show ${sfc_port} -f value -c fixed_ips|grep "ip_address='[0-9]*\."|cut -d"'" -f2) <<EOF
+sudo sh -c 'echo "auto eth1" >> /etc/network/interfaces'
+sudo sh -c 'echo "iface eth1 inet dhcp" >> /etc/network/interfaces'
+sudo /etc/init.d/S40network restart
+sudo sh -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
+sudo ip route add ${SOURCE_IP} dev eth0
+sudo ip route add ${DEST_IP} dev eth1
+
+EOF
+done
+
+
 # create captures
 ## T3
 SKYDIVE_USERNAME=admin SKYDIVE_PASSWORD=pass123456 /opt/stack/go/bin/skydive --conf /tmp/skydive.yaml client capture create --name SFCFirewall11 --gremlin "G.V().Has('Neutron/IPs', Regex('10.0.0.11,*'))"
